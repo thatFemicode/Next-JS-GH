364618992d9f3e87d8ee37c4d433bf5dcf1457dc
thatFemicode <leyedavid10@gmail.com>
2022-05-04 19:07:55 +0100

    services api

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import axios from "axios";
+
+import { useState, useEffect } from "react";
 export default function Home(props) {
   // console.log(value);
   return (
---
+++ services/githubService.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const axiosConfig = {
+  baseUrl: "https://api.github.com/",
+  auth: {
+    username: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
+    password: process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET,
+  },
+};
+
+// this will accept two argeuments
+function searchRepos(searchText, language) {
+    const query = language ? `${searchText}+language:${language}` : searchText;
+  
+    return axios.get(
+      `search/repositories?q=${query}&sort=stars&order=desc`,
+      axiosConfig
+    );
+  }
+  
+  export { searchRepos };
