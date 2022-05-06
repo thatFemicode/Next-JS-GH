d0951eff7cc640ce2c53d430df9cf65d4154b1db
thatFemicode <leyedavid10@gmail.com>
2022-05-06 23:39:05 +0100

    cancel config

--- services/githubService.js
+++ services/githubService.js
@@ -1,20 +1,25 @@
 import axios from "axios";
+import { axiosGetCancellable } from "helpers/axiosHelpers";
 
-const axiosConfig = axios.create({
+const axiosConfig = {
   baseURL: "https://api.github.com",
   auth: {
     username: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
     password: process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET,
   },
-});
+};
 
 // this will accept two argeuments
 function searchRepos(searchText, language) {
   const query = language ? `${searchText}+language:${language}` : searchText;
 
-  return axiosConfig.get(
-    `search/repositories?q=${query}&sort=stars&order=desc`
-    // `/search/repositories?q=tetris+language:assembly&sort=stars&order=desc`
+  // return axiosConfig.get(
+  //   `search/repositories?q=${query}&sort=stars&order=desc`, axiosConfig
+  //   // `/search/repositories?q=tetris+language:assembly&sort=stars&order=desc`
+  // );
+  return axiosGetCancellable(
+    `search/repositories?q=${query}&sort=stars&order=desc`,
+    axiosConfig
   );
 }
 
