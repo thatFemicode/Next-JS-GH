0dc50b3b72b8bc2600cde2f4baa6392dedad89c5
thatFemicode <leyedavid10@gmail.com>
2022-05-09 21:32:52 +0100

    modified button

--- Components/Button/Button.jsx
+++ Components/Button/Button.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import Link from "next/link";
+import styles from "./button.module.scss";
+const Button = ({ href, text, type = "primary", target, external }) => {
+  const types = {
+    primary: "is-primary",
+    success: "is-success",
+    danger: "is-danger",
+    dark: "is-dark",
+    light: "is-light",
+  };
 
-const Button = () => {
-  return <div>Button</div>;
+  const buttonClass = `button ${types[type]}`;
+
+  if (external) {
+    return (
+      <div className={styles.button}>
+        <a className={buttonClass} target={target} href={href}>
+          {text}
+        </a>
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.button}>
+      <Link href={href}>
+        <a className={buttonClass} target={target}>
+          {text}
+        </a>
+      </Link>
+    </div>
+  );
 };
 
 export default Button;
